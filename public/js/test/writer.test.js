63f225114b1adc6d70e9fe6ba841c7a3149d8968
pwdd <pwdd@users.noreply.github.com>
2017-02-04 19:16:42 -0600

    Add tests for boardString

--- public/js/test/writer.test.js
+++ public/js/test/writer.test.js
@@ -1,10 +1,42 @@
 const writer = require('../src/writer');
 
-test('add 1 + 2 to equal 3', () => {
+describe("boardString", () => {
   var json = {
     "board": ["_","_", "_", "_", "_", "_", "_", "_", "_"],
     "spot": 0
   };
-  expect(writer.boardString(json)).toMatch("_,_,_,_,_,_,_,_,_");
-});
+
+  var form = writer.boardString(json);
+
+  var boardLength = json["board"].length;
+
+  function countOcurrenciesOf(string) {
+    var re = new RegExp(string, "g");
+    return (form.match(re) || []).length
+  }
+
+  test('contains a representation of the board', () => {
+    expect(form).toMatch('value="_,_,_,_,_,_,_,_,_"');
+  });
+
+  test("contains the value of spot", () => {
+    expect(form).toMatch('value="0"')
+  });
+
+  test("contains 9 <form> tags", () => {
+    expect(countOcurrenciesOf("</form>")).toEqual(boardLength);
+  });
+
+  test("contains 3 <br> tags", () => {
+    expect(countOcurrenciesOf("<br>")).toEqual(Math.sqrt(boardLength));
+  });
+
+  test("all the forms contain the value 'board'", () => {
+    expect(countOcurrenciesOf("name=\"board\"")).toEqual(boardLength);
+  });
+
+  test("all the forms contain the value of spot", () => {
+    expect(countOcurrenciesOf("name=\"spot\"")).toEqual(boardLength);
+  })
+})
 
